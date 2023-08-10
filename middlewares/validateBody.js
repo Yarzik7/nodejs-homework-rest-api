0815d1847107c5fdce1f90014ab3091a24a30abd
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-08-10 23:53:53 +0300

    Remove console.log

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -8,7 +8,6 @@ const { httpError } = require('../helpers');
 const validateBody = schema => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
-    console.log(error);
     if (error) {
       next(httpError(400, `missing required ${error.details[0].path[0]} field`));
     }
