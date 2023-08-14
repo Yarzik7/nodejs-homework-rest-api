eeb3cdf233decddc7081920cbe06d1dd7ddbac60
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-08-14 21:54:16 +0300

    Refactoring

--- helpers/index.js
+++ helpers/index.js
@@ -1,12 +1,10 @@
 const Errors = require('./Errors');
-const generateErrorByType = require('./generateErrorByType')
 const ctrlWrapper = require('./ctrlWrapper');
 const operationById = require('./operationById');
 const handleMongooseError = require('./handleMongooseError');
 
 module.exports = {
   Errors,
-  generateErrorByType,
   ctrlWrapper,
   operationById,
   handleMongooseError,
